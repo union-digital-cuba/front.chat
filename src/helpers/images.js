d156782d466b92f9c2d2925aba4e36856c36d20b
Alberto Carlos Carbonell Marcé <accarbonell1987@gmail.com>
2022-10-28 19:49:16 -0400

    Pegandole a una API para coger el grupo publico

--- src/helpers/images.js
+++ src/helpers/images.js
@@ -7,8 +7,6 @@ const ConvertToBase64 = (data) => {
 
 const GetImage = (image) => {
   return image && `data:image/svg+xml;base64,${image}`
-
-  // return type === CustomTypes.ImageTypes.web ? `data:image/svg+xml;base64,${image}` : `images/avatars/${image}`
 }
 
 export { ConvertToBase64, GetImage }
