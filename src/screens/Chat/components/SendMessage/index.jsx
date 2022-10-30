ea838d8d1294bfacc101431cb9bd8faa0eaeac94
Alberto Carlos Carbonell Marcé <accarbonell1987@gmail.com>
2022-10-29 21:42:09 -0400

    Comenzando Notification Area

--- src/screens/Chat/components/SendMessage/index.jsx
+++ src/screens/Chat/components/SendMessage/index.jsx
@@ -9,17 +9,20 @@ import { SendIcon } from 'components/Icons'
 const ChatSendMessage = () => {
   return (
     <div className="send-message-container">
-      <Input
-        css={{ w: '100%' }}
-        clearable
-        contentRightStyling={false}
-        placeholder="Type your message..."
-        contentRight={
-          <SendButton>
-            <SendIcon />
-          </SendButton>
-        }
-      />
+      <div className="send-message-input">
+        <Input
+          css={{ w: '100%' }}
+          clearable
+          contentRightStyling={false}
+          placeholder="Type your message..."
+          contentRight={
+            <SendButton>
+              <SendIcon />
+            </SendButton>
+          }
+          // onPointerDown={() => alert('entrado')}
+        />
+      </div>
     </div>
   )
 }
